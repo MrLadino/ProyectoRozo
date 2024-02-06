3946db17dd23067e571c7694de570b87f61a1544
Luis-005 <bermudezluisperezangel@gmail.com>
2024-02-06 14:21:41 -0500

    more changes

---
+++ react-intro/src/App.jsx
@@ -0,0 +1,17 @@
+import { Example } from './Components/Example/Example'
+import './App.css'
+
+
+
+
+function App() {
+  
+  return (
+    <>
+    <Example/>
+    <Example title = 'Andres homosexual de mierda' />
+    </>
+  )
+}
+
+export default App
---
+++ react-intro/src/Components/Example/Example.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Example({title = 'hello dinasaurio t rex'}) {
+  return (
+    <div>
+        <h2>title</h2>
+        <h2>title</h2>
+    </div>
+  )
+}
